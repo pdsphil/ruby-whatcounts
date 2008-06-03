af653611e844c4e68dba8d7feb2e7b7ae91da645
Phil R <phil@pdatasolutions.com>
2008-06-03 17:01:53 -0600

    new directory structure and license file

--- what_counts.rb
+++
@@ -1,4 +0,0 @@
-require File.dirname(__FILE__) + "/base"
-require File.dirname(__FILE__) + "/what_counts/base"
-require File.dirname(__FILE__) + "/what_counts/api_request"
-require File.dirname(__FILE__) + "/what_counts/api_response"
